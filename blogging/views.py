95812011e508f2715bf64583e53753a5c5809d63
Mercedes Gonzalez Gonzalez <mgglez@uw.edu>
2022-11-10 23:22:43 -0600

    adding class based views to blogging app - Assignment for Lesson08

--- blogging/views.py
+++ blogging/views.py
@@ -3,30 +3,14 @@ from django.http import HttpResponse, Http404
 from django.template import loader
 from blogging.models import Post
 
-def stub_view(request, *args, **kwargs):
-    body = "Stub View\n\n"
-    if args:
-        body += "Args:\n"
-        body += "\n".join(["\t%s" % a for a in args])
-    if kwargs:
-        body += "Kwargs:\n"
-        body += "\n".join(["\t%s: %s" % i for i in kwargs.items()])
-    return HttpResponse(body, content_type="text/plain")
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
-def list_view(request):
-    published = Post.objects.exclude(published_date__exact=None)
-    posts = published.order_by('-published_date')
-    # template = loader.get_template('blogging/list.html')
-    context = {'posts': posts}
-    # body = template.render(context)
-    # return HttpResponse(body, content_type="text/html")
-    return render(request, 'blogging/list.html', context)
+class PostListView(ListView):
+    queryset = Post.objects.exclude(published_date__exact=None).order_by('-published_date')
+    template_name = 'blogging/list.html'
 
-def detail_view(request, post_id):
-    published = Post.objects.exclude(published_date__exact=None)
-    try:
-        post = published.get(pk=post_id)
-    except Post.DoesNotExist:
-        raise Http404
-    context = {'post': post}
-    return render(request, 'blogging/detail.html', context)
+
+class PostDetailView(DetailView):
+    queryset = Post.objects.exclude(published_date__exact=None).order_by('-published_date')
+    template_name = 'blogging/detail.html'
